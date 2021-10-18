8c6a6f8912c5b396ba6bcbc6977fb2e2c8761a4b
EC2 Default User <ec2-user@ip-172-31-7-89.us-east-2.compute.internal>
2021-10-18 19:30:55 +0000

    “Complete”

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,3 +1,24 @@
 class Movie < ActiveRecord::Base
+    def self.movie_with_id(id)
+        Movie.where('id':id).pluck(:title)[0]
+    end
+    
+    def self.similar_to(id)
+        director=Movie.where('id':id).pluck(:director)[0]
+        if (director=='' || director==nil)
+            return ''
+        else
+            return Movie.where('director':director)
+        end
+    end
+    
+    def self.all_ratings
+        self.distinct.pluck(:rating)
+    end
+    
+    def self.with_ratings(ratings_list, to_sort)
+        ratings_list.map!(&:upcase)
+        ratings_list.nil? ? self.all.order(to_sort) : self.where('rating': ratings_list).order(to_sort)
+    end
 
 end
--- rottenpotatoes/features/step_definitions/movies_steps.rb
+++ rottenpotatoes/features/step_definitions/movies_steps.rb
@@ -1,9 +1,14 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    Movie.create movie
+    Movie.create!(movie)
   end
 end
+
+Then /(.*) seed movies should exist/ do | n_seeds |
+  Movie.count.should be n_seeds.to_i
+end
+
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
@@ -11,11 +16,31 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   expect(page.body.index(e1) < page.body.index(e2))
 end
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  rating_list.split(', ').each do |rating|
-    step %{I #{uncheck.nil? ? '' : 'un'}check "ratings_#{rating}"}
+When /I (un)?check the following ratings: +"(.*)"$/ do |uncheck, rating_list|
+  ratings = rating_list.split;
+  ratings.each do |r|
+    if (uncheck)
+      uncheck("ratings_#{r}")
+    else
+      check("ratings_#{r}")
+    end
   end
 end
+
+Then /I should (not )?see the following ratings: +"(.*)"$/ do |yes, ratings|
+  hash = Hash[(ratings.split).map {|rating| [rating, 1]} ]
+  mat = "//table[@id='movies']//td[2]"
+  page.all(:xpath, mat) do |r|
+      if yes
+        if (hash.key?(r.text)) 
+          fail "Not expected but found: #{r}"
+        end
+      else
+        if !(hash.key?(r.text))
+          fail "Expected but not found: #{r}"
+        end
+      end
+  end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
@@ -24,7 +49,12 @@ Then /I should see all the movies/ do
   end
 end
 
-Then /the director of "(.*)" should be "(.*)"/ do |movie_title, director|
-  movie = Movie.find_by(title: movie_title)
-  expect(movie.director == director)
+Then /the director of "(.*)" should be "(.*)"/ do |movie, director|
+  if page.respond_to? :should
+    page.should have_content("Details about #{movie}")
+    page.should have_content("Director: #{director}")
+  else
+    assert page.has_content?("Details about #{movie}")
+    assert page.has_content?("Director: #{director}")
+  end
 end
